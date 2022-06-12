bb6e700b2a273f503bcdd3ff1966077d6c8b9d36
nextlevelplay <Kgnpdknga2703>
2022-06-12 04:21:21 +0300

    add script

---
+++ PentahoDataIntegrationTool/WORKFOLDER/script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -L -o /Users/pavelsfomins/Desktop/Data-Engineering/PentahoDataIntegrationTool/HOME/sample-superstore-shell.xls https://github.com/nextlevelplay/Data-Engineering/raw/main/DataBaseExample/Sample%20-%20Superstore.xls
